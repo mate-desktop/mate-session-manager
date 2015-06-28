47ddcc32cdfc5ce1ec0b39f7b5df52a3ca07a7e6
OBATA Akio <obache@users.noreply.github.com>
2015-06-28 16:16:39 +0900

    Fix uninitialized variable usages for !HAVE_UPOWER

--- mate-session/gsm-manager.c
+++ mate-session/gsm-manager.c
@@ -3385,8 +3385,8 @@ gsm_manager_can_shutdown (GsmManager *manager,
 #ifdef HAVE_SYSTEMD
         GsmSystemd *systemd;
 #endif
-        gboolean can_suspend;
-        gboolean can_hibernate;
+        gboolean can_suspend = FALSE;
+        gboolean can_hibernate = FALSE;
 #ifdef HAVE_UPOWER
         g_object_get (manager->priv->up_client,
                       "can-suspend", &can_suspend,
