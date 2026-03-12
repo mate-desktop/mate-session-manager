311fd9d2dc7540ac3db97107cb68d1b94931e259
mbkma <39454100+mbkma@users.noreply.github.com>
2026-03-12 18:13:32 +0100

    ci: migrate from Travis CI to GitHub Actions
    Replace Travis CI configuration with GitHub Actions workflows.
    Add CI build workflow for Debian, Fedora, Ubuntu, and Archlinux.
    Add release workflow for automated GitHub releases on tags.
    Add dependabot configuration for GHA pin updates.
    Remove obsolete .travis.yml.
    remove .build.yml

---
+++ .github/workflows/ubuntu.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/bash
+
+set -eo pipefail
+
+# Use grouped output messages
+infobegin() {
+	echo "::group::${1}"
+}
+infoend() {
+	echo "::endgroup::"
+}
+
+# Required packages on Ubuntu
+requires=(
+	ccache # Use ccache to speed up build
+)
+
+requires+=(
+	autoconf-archive
+	autopoint
+	gcc
+	git
+	intltool
+	libdbus-glib-1-dev
+	libgl1-mesa-dev
+	libgles2-mesa-dev
+	libglib2.0-dev
+	libgtk-3-dev
+	libice-dev
+	libsm-dev
+	libstartup-notification0-dev
+	libsystemd-dev
+	libx11-dev
+	libxau-dev
+	libxext-dev
+	libxrender-dev
+	libxt-dev
+	libxtst-dev
+	make
+	mate-common
+	xmlto
+	xsltproc
+)
+
+infobegin "Update system"
+apt-get update -y
+infoend
+
+infobegin "Install dependency packages"
+env DEBIAN_FRONTEND=noninteractive \
+	apt-get install --assume-yes \
+	${requires[@]}
+infoend
